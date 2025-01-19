71fe85bb97e90b38659f0efab4e7af50e64c0cf9
Artmines <96462463+Artmines@users.noreply.github.com>
2025-01-19 07:11:51 -0700

    Refactor bridge

--- modules/bridge/rsg/client.lua
+++ modules/bridge/rsg/client.lua
@@ -5,59 +5,70 @@ local Weapon = require 'modules.weapon.client'
 RegisterNetEvent('RSGCore:Client:OnPlayerUnload', client.onLogout)
 
 RegisterNetEvent('RSGCore:Player:SetPlayerData', function(data)
-	if source == '' or not PlayerData.loaded then return end
+    if source == '' or not PlayerData.loaded then return end
 
-	if (data.metadata.isdead or data.metadata.inlaststand) ~= PlayerData.dead then
-		PlayerData.dead = data.metadata.isdead or data.metadata.inlaststand
-		OnPlayerData('dead', PlayerData.dead)
-	end
+    if (data.metadata.isdead or data.metadata.inlaststand) ~= PlayerData.dead then
+        PlayerData.dead = data.metadata.isdead or data.metadata.inlaststand
+        OnPlayerData('dead', PlayerData.dead)
+    end
 
-	local groups = PlayerData.groups
+    local groups = PlayerData.groups
 
-	if not groups[data.job.name] or not groups[data.gang.name] or groups[data.job.name] ~= data.job.grade.level or groups[data.gang.name] ~= data.gang.grade.level then
-		PlayerData.groups = {
-			[data.job.name] = data.job.grade.level,
-			[data.gang.name] = data.gang.grade.level,
-		}
+    if not groups[data.job.name] or not groups[data.gang.name] or groups[data.job.name] ~= data.job.grade.level or groups[data.gang.name] ~= data.gang.grade.level then
+        PlayerData.groups = {
+            [data.job.name] = data.job.grade.level,
+            [data.gang.name] = data.gang.grade.level,
+        }
 
-		OnPlayerData('groups', PlayerData.groups)
-	end
-end)
+        OnPlayerData('groups', PlayerData.groups)
+    end
 
-RegisterNetEvent('police:client:GetCuffed', function()
-	PlayerData.cuffed = not PlayerData.cuffed
-	LocalPlayer.state:set('invBusy', PlayerData.cuffed, false)
-
-	if not PlayerData.cuffed then return end
-
-	Weapon.Disarm()
+    if data.metadata.ishandcuffed then
+        PlayerData.cuffed = true
+        LocalPlayer.state:set('invBusy', true, false)
+        Weapon.Disarm()
+    elseif PlayerData.cuffed then
+        PlayerData.cuffed = false
+        LocalPlayer.state:set('invBusy', false, false)
+    end
 end)
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function client.setPlayerStatus(values)
-	for name, value in pairs(values) do
+    for name, value in pairs(values) do
+        -- compatibility for ESX style values
+        if value > 100 or value < -100 then
+            value = value * 0.0001
+        end
 
-		-- compatibility for ESX style values
-		if value > 100 or value < -100 then
-			value = value * 0.0001
-		end
-
-		if name == "hunger" then
-			TriggerServerEvent('consumables:server:addHunger', RSGCore.Functions.GetPlayerData().metadata.hunger + value)
-		elseif name == "thirst" then
-			TriggerServerEvent('consumables:server:addThirst', RSGCore.Functions.GetPlayerData().metadata.thirst + value)
-		elseif name == "stress" then
-			if value > 0 then
-				TriggerServerEvent('hud:server:GainStress', value)
-			else
-				value = math.abs(value)
-				TriggerServerEvent('hud:server:RelieveStress', value)
-			end
-		end
-	end
+        if name == "hunger" then
+            TriggerServerEvent('consumables:server:addHunger', RSGCore.Functions.GetPlayerData().metadata.hunger + value)
+        elseif name == "thirst" then
+            TriggerServerEvent('consumables:server:addThirst', RSGCore.Functions.GetPlayerData().metadata.thirst + value)
+        elseif name == "stress" then
+            if value > 0 then
+                TriggerServerEvent('hud:server:GainStress', value)
+            else
+                value = math.abs(value)
+                TriggerServerEvent('hud:server:RelieveStress', value)
+            end
+        end
+    end
 end
 
-local function hasItem(items, amount)
+AddStateBagChangeHandler('inv_busy', ('player:%s'):format(cache.serverId), function(_, _, value)
+    LocalPlayer.state:set('invBusy', value, false)
+end)
+
+local function export(exportName, func)
+    AddEventHandler(('__cfx_export_%s_%s'):format(string.strsplit('.', exportName, 2)), function(setCB)
+        setCB(func or function()
+            error(("export '%s' is not supported when using QC-Inventory"):format(exportName))
+        end)
+    end)
+end
+
+export('rsg-inventory.HasItem', function(items, amount)
     amount = amount or 1
 
     if type(items) == 'table' then
@@ -71,12 +82,4 @@ local function hasItem(items, amount)
     else
         return Inventory.GetItemCount(items) >= amount
     end
-end
-
-AddStateBagChangeHandler('inv_busy', ('player:%s'):format(cache.serverId), function(_, _, value)
-	LocalPlayer.state:set('invBusy', value, false)
-end)
-
-AddEventHandler(('__cfx_export_rsg-inventory_HasItem'), function(setCB)
-	setCB(hasItem)
 end)
