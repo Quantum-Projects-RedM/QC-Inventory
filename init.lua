53a1525537a65f75ca8e32120a88e1f93cf988ec
Artmines <96462463+Artmines@users.noreply.github.com>
2025-01-19 18:08:44 -0700

    fix init.lua

--- init.lua
+++ init.lua
@@ -92,7 +92,7 @@ else
         weaponmismatch = GetConvarInt('inventory:weaponmismatch', 1) == 1,
         ignoreweapons = json.decode(GetConvar('inventory:ignoreweapons', '[]')),
         suppresspickups = GetConvarInt('inventory:suppresspickups', 1) == 1,
-        weaponWheel = GetConvarInt('inventory:weaponWheel', true),
+        weaponWheel = GetConvarInt('inventory:weaponWheel', 0) == 1,
     }
 
     local ignoreweapons = table.create(0, (client.ignoreweapons and #client.ignoreweapons or 0) + 3)
