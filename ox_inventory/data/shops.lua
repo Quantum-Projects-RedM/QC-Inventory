52842bcc9c4b04812ffcd1dc14e35dc2321120ca
Artmines <96462463+Artmines@users.noreply.github.com>
2025-07-17 15:39:16 -0230

    All shops pushed

--- ox_inventory/data/shops.lua
+++ ox_inventory/data/shops.lua
@@ -22,8 +22,8 @@
 --]]
 
 return {
-    General = {
-        name = 'General Store',
+    General_Rhodes = {
+        name = 'Rhodes General Store',
         id = '1',
         promptKey = 'J', -- Check rsg-core/shared/keybinds.lua for the prompt key.
         blip = {
@@ -37,11 +37,107 @@ return {
         },
         locations = {
             vec3(1328.09, -1293.74, 77.02),   -- Rhodes General Store
+        }
+    },
+    General_Valentine = {
+        name = 'Valentine General Store',
+        id = '2',
+        promptKey = 'J', -- Check rsg-core/shared/keybinds.lua for the prompt key.
+        blip = {
+            id = 'blip_shop_store', 
+            colour = "BLIP_MODIFIER_MP_COLOR_32", 
+            scale = 0.2
+        },
+        inventory = {
+            { name = "water",          price = 1.0, count = 50 },
+            { name = "bread",          price = 1.0, count = 50 },
+        },
+        locations = {
             vec3(-322.36, 803.84, 117.88),    -- Valentine General Store
-            vec3(-1791.22, -387.08, 160.33),  -- Strawberry General Store
+        }
+    },    
+    General_SaintDenis = {
+        name = 'Saint Denis General Store',
+        id = '3',
+        promptKey = 'J', -- Check rsg-core/shared/keybinds.lua for the prompt key.
+        blip = {
+            id = 'blip_shop_store', 
+            colour = "BLIP_MODIFIER_MP_COLOR_32", 
+            scale = 0.2
+        },
+        inventory = {
+            { name = "water",          price = 1.0, count = 50 },
+            { name = "bread",          price = 1.0, count = 50 },
+        },
+        locations = {
             vec3(2825.59, -1318.19, 46.76),   -- Saint Denis General Store
-            vec3(-5487.24, -2938.99, -0.39),  -- Tumbleweed General Store
+        }
+    },
+    General_Blackwater = {
+        name = 'Blackwater General Store',
+        id = '4',
+        promptKey = 'J', -- Check rsg-core/shared/keybinds.lua for the prompt key.
+        blip = {
+            id = 'blip_shop_store', 
+            colour = "BLIP_MODIFIER_MP_COLOR_32", 
+            scale = 0.2
+        },
+        inventory = {
+            { name = "water",          price = 1.0, count = 50 },
+            { name = "bread",          price = 1.0, count = 50 },
+        },
+        locations = {
             vec3(-785.11, -1323.83, 43.86),   -- Blackwater General Store
+        }
+    },
+    General_Tumbleweed = {
+        name = 'Tumbleweed General Store',
+        id = '5',
+        promptKey = 'J', -- Check rsg-core/shared/keybinds.lua for the prompt key.
+        blip = {
+            id = 'blip_shop_store', 
+            colour = "BLIP_MODIFIER_MP_COLOR_32", 
+            scale = 0.2
+        },
+        inventory = {
+            { name = "water",          price = 1.0, count = 50 },
+            { name = "bread",          price = 1.0, count = 50 },
+        },
+        locations = {
+            vec3(-5487.24, -2938.99, -0.39),  -- Tumbleweed General Store
+        }
+    },
+    General_Strawberry = {
+        name = 'Strawberry General Store',
+        id = '6',
+        promptKey = 'J', -- Check rsg-core/shared/keybinds.lua for the prompt key.
+        blip = {
+            id = 'blip_shop_store', 
+            colour = "BLIP_MODIFIER_MP_COLOR_32", 
+            scale = 0.2
+        },
+        inventory = {
+            { name = "water",          price = 1.0, count = 50 },
+            { name = "bread",          price = 1.0, count = 50 },
+        },
+        locations = {
+            vec3(-1791.22, -387.08, 160.33),  -- Strawberry General Store
+        }
+    },
+    General_VanHorn = {
+        name = 'Van Horn General Store',
+        id = '7',
+        promptKey = 'J', -- Check rsg-core/shared/keybinds.lua for the prompt key.
+        blip = {
+            id = 'blip_shop_store', 
+            colour = "BLIP_MODIFIER_MP_COLOR_32", 
+            scale = 0.2
+        },
+        inventory = {
+            { name = "water",          price = 1.0, count = 50 },
+            { name = "bread",          price = 1.0, count = 50 },
+        },
+        locations = {
             vec3(3027.29, 562.30, 44.72),     -- Van Horn General Store
         }
     },
